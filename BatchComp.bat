ccbab85c77cbea7f80e0d5616cbd1c6ff85144d8
lawrenceli79 <lawrenceli79@gmail.com>
2025-01-13 19:00:16 +0800

    v1a

--- BatchComp.bat
+++ BatchComp.bat
@@ -1,6 +1,12 @@
 @echo off
-py CompHokenTxt_Calc.py org\hoken_202401.txt new1\hoken_202401.txt
-py CompHokenTxt_Calc.py org\hoken_202402.txt new1\hoken_202402.txt
-py CompHokenTxt_Calc.py org\hoken_202403.txt new1\hoken_202403.txt
-py CompHokenTxt_Calc.py org\hoken_202408.txt new1\hoken_202408.txt
-py CompHokenTxt_Calc.py org\hoken_202409.txt new1\hoken_202409.txt
+py CompHokenTxt_Calc.py org\hoken_202401.txt Deal32\hoken_202401.txt
+py CompHokenTxt_Calc.py org\hoken_202402.txt Deal32\hoken_202402.txt
+py CompHokenTxt_Calc.py org\hoken_202403.txt Deal32\hoken_202403.txt
+py CompHokenTxt_Calc.py org\hoken_202408.txt Deal32\hoken_202408.txt
+py CompHokenTxt_Calc.py org\hoken_202409.txt Deal32\hoken_202409.txt
+
+py CompHokenTxt_Calc.py org\hoken_202401.txt Deal2023\hoken_202401.txt
+py CompHokenTxt_Calc.py org\hoken_202402.txt Deal2023\hoken_202402.txt
+py CompHokenTxt_Calc.py org\hoken_202403.txt Deal2023\hoken_202403.txt
+py CompHokenTxt_Calc.py org\hoken_202408.txt Deal2023\hoken_202408.txt
+py CompHokenTxt_Calc.py org\hoken_202409.txt Deal2023\hoken_202409.txt
